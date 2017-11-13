05d850863d99778010d4b1a5f635cef7bc71196d
Evanw89 <evanw89@gmail.com>
2017-11-12 16:03:10 -0800

    updated with client photos and copy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 function navChange() {
-	$("#navbar").addClass("bg-darkblue");
-}
+	$("#navbar").addClass("bg-darkblue py-4");
+};
 $(".grid img").hover(function() {
 	$(this).css("opacity", "0")
 }, function() {
 	$(this).css("opacity", "1")
-})
+});
+
+// TweenMax.to("#loader", 1, {opacity:0, ease:Expo.easeIn, delay: 3})
